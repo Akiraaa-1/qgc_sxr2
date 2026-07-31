ede26c242d200e0d50f57e0fda3067d97ef9688f
Akiraaa-1 <Akiraaa-1@users.noreply.github.com>
2026-07-31 17:50:02 +0800

    Build Windows package as BTFW-GCS

--- cmake/install/Install.cmake
+++ cmake/install/Install.cmake
@@ -134,11 +134,7 @@ elseif(LINUX)
 # ----------------------------------------------------------------------------
 elseif(WIN32)
     # Pass variables to Windows installer creation script
-    if(CMAKE_CROSSCOMPILING)
-        set(_win_installer_out "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}-installer-${CMAKE_HOST_SYSTEM_PROCESSOR}-${CMAKE_SYSTEM_PROCESSOR}.exe")
-    else()
-        set(_win_installer_out "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}-installer-${CMAKE_SYSTEM_PROCESSOR}.exe")
-    endif()
+    set(_win_installer_out "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.exe")
     install(CODE "
         set(CMAKE_PROJECT_NAME \"${CMAKE_PROJECT_NAME}\")
         set(CMAKE_PROJECT_VERSION \"${CMAKE_PROJECT_VERSION}\")
